efec959f7de0ffce0ecd7dfd817c7372a1051bbe
Étienne Gauvin <etienne@gauvin.info>
2017-03-20 14:49:09 +0100

    Realistic movement of the needle

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", (event) => {
 
 	const compassElement = document.querySelector('.compass')
-	const needleElement = document.querySelector('.compass .needle')
+	const needleElement = document.querySelector('.compass .red.needle')
+	const greenNeedleElement = document.querySelector('.compass .green.needle')
 
 	if ('geolocation' in navigator) {
 		window.navigator.geolocation.watchPosition(
@@ -33,20 +34,71 @@ document.addEventListener("DOMContentLoaded", (event) => {
 		window.alert("La boussole n'est pas disponible.")
 	}
 
+	// Dernier angle donné
+	let lastRawAngle = 0
+
+	// Angle visuel de l'aiguille
 	let angle = 0
 
-	function onCompassUpdate(normalizedAngle) {
+	// Vélocité de l'aiguille
+	let velocity = 0
+
+	/**
+	 * Nouvel angle donné par le device
+	 */
+	function onCompassUpdate(rawAngle) {
+		const now = +new Date
 
 		// Différence entre les deux derniers angles
-		let diff = normalizedAngle - angle
-		diff = ((diff + 180) - Math.floor((diff + 180) / 360) * 360) - 180
+		let diff = getDiffBetweenAngles(rawAngle, lastRawAngle)
 
 		// Nouvel angle -+Infini
 		// pour éviter les transitions étranges (l'aiguille qui fait le tour du cadran, par ex.)
-		angle += diff
+		lastRawAngle += diff
 
-		needleElement.style.transitionDuration = `${Math.abs(diff) * 8}ms`
+		//greenNeedleElement.style.transform = `rotate(${lastRawAngle}deg)`
+	}
+
+	/** 
+	 * Mise à jour de la vélocité de l'aiguille
+	 * Séparée de la requestAnimationFrame pour ne pas surcharger
+	 */
+	function updateVelocity() {
+		const diff = getDiffBetweenAngles(lastRawAngle, angle)
+		velocity = velocity * 0.5 + diff * 2
+	}
+
+	// Dernier instant enregistré d'affichage d'une frame
+	let previousTime = +new Date
+
+	/**
+	 * Mise à jour visuelle de l'aiguille
+	 * @param <Number> time
+	 */
+	function updateNeedle(time) {
+
+		// Changement d'angle en fonction de la vélocité
+		angle += velocity * ((time - previousTime) / 1000)
+
+		// Application à l'élément visuel
 		needleElement.style.transform = `rotate(${angle}deg)`
+
+		previousTime = time
+		window.requestAnimationFrame(updateNeedle)
+	}
+
+	window.requestAnimationFrame(updateNeedle)
+	setInterval(updateVelocity, 200)
+
+	/**
+	 * Calcul de la différence entre les deux angles (360)
+	 * @param <Number> a
+	 * @param <Number> b
+	 * @return <Number>
+	 */
+	function getDiffBetweenAngles(a, b) {
+		let diff = a - b + 180
+		return (diff - Math.floor(diff / 360) * 360) - 180
 	}
 
 })
